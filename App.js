c2b6d56a2c6201f4c9a33344251fed8bb506c093
ikhowal2004 <119477527+ikhowal2004@users.noreply.github.com>
2025-03-25 13:57:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Greeting from "./Greeting"; // Importing the Greeting component
+
+const App = () => {
+    return <Greeting name="John" />;
+};
+
+export default App;
---
+++ Greeting.js
@@ -0,0 +1,5 @@
+const Greeting = (props) => {
+    return <h2>Hello, {props.name}!</h2>;
+};
+
+export default Greeting;
